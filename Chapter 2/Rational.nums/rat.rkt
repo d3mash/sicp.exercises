6717ceab3f46e87bc9dda5e153cb3f8f17669df4
Demid Kashirin <demid.kashirin@gmail.com>
2018-03-21 01:34:30 +0300

    Added primitives for rational numbers

---
+++ Chapter 2/Rational.nums/rat.rkt
@@ -0,0 +1,37 @@
+#lang racket
+(require "pairs.rkt")
+(require (file "../../general/primitives.rkt"))
+
+; introducing rational numbers abstraction via built-in cons car and cdr
+(define (make-rat n d) 
+  (let ((g (* (gcd (abs n) (abs d)) (sign d)))) 
+    (cons (/ n g) (/ d g))))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+; simple operations with rational numbers
+; presuming we alreadey have (make-rat) (numer) and (denom)
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+(define (equal-rat x y)
+  (= (* (numer x) (denom y)) (* (numer y) (denom x))))
+
+(provide (all-defined-out))
