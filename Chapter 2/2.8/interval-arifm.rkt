0429a47f9b010630112bb190e8dd5f3868eed7c8
Demid Kashirin <demid.kashirin@gmail.com>
2018-04-18 12:17:10 +0300

    fix arcitecture

---
+++ Chapter 2/2.8/interval-arifm.rkt
@@ -0,0 +1,25 @@
+#lang racket
+(require (file "../2.7/intervals.rkt"))
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (lower-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x (make-interval (/ 1.0 (upper-bound y))
+                                 (/ 1.0 (lower-bound y)))))
+
+; define a procedure which calculates sub for two intervals
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+(provide (all-defined-out))
+  
