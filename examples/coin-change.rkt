c2177e58d5e5f06b78f367e5e4d9d7493cd24f53
Demid Kashirin <demid.kashirin@gmail.com>
2018-04-19 18:12:40 +0300

    add an obsolete coin-change solution

---
+++ examples/coin-change.rkt
@@ -0,0 +1,25 @@
+#lang racket
+; split a dollar using 50 25 10 5 1 -cent coins 
+
+(define (count-change amount)
+  (cc amount 5))
+
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+
+
+(count-change 100)
