a6556f96c871c8c4e891288249fd58002c251778
Demid Kashirin <demid.kashirin@gmail.com>
2018-04-19 15:07:25 +0300

    add an interface for basic list operations

---
+++ general/lists.rkt
@@ -0,0 +1,19 @@
+#lang racket
+(define (list-ref list n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define (length list)
+  (define (iter a count)
+    (if (null? a)
+        count
+        (iter (cdr a) (+ 1 count))))
+  (iter list 0))
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list 2))))
+
+
