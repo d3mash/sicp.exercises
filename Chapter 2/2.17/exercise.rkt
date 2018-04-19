bc359ffe025ab9abe03e5d92f6977d7c391ac97f
Demid Kashirin <demid.kashirin@gmail.com>
2018-04-19 15:41:36 +0300

    fix test

--- Chapter 2/2.17/exercise.rkt
+++ Chapter 2/2.17/exercise.rkt
@@ -1,7 +1,9 @@
 #lang racket
 (define (last-pair list)
   (if (null? list)
-      (error "empty list")
+      null
       (if (null? (cdr list))
           (car list)
           (last-pair (cdr list)))))
+
+(provide last-pair)
--- Chapter 2/2.17/test.rkt
+++ Chapter 2/2.17/test.rkt
@@ -3,5 +3,7 @@
 (require (file "exercise.rkt"))
 (require test-engine/racket-tests)
 (define squares (list 1 4 9 16 25 36 49 64 81 100))
-(check-expect (last-pair squares) '(100))
+(define empty-list null)
+(check-expect (last-pair squares) 100)
+(check-expect (last-pair empty-list) null)
 (test)
