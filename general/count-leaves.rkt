af9b79c4a868020de55c72ce38a1f8a9f5312909
Demid Kashirin <demid.kashirin@gmail.com>
2018-04-24 00:00:12 +0300

    add interface for countint leaves

--- general/count-leaves.rkt
+++ general/count-leaves.rkt
@@ -4,6 +4,4 @@
         ((not (pair? x)) 1)
         (else (+ (count-leaves (car x))
                  (count-leaves (cdr x))))))
-
-(count-leaves (cons (list 1 2) (list 3 4)))
-(length (cons (list 1 2) (list 3 4)))
+(provide count-leaves)
