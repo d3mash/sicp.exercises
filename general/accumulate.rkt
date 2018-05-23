cc3df950ef44c8a8a1cef3a58a8aa869e4d62676
Demid Kashirin <demid.kashirin@gmail.com>
2018-05-23 17:00:14 +0300

    add an accumulate primitive

---
+++ general/accumulate.rkt
@@ -0,0 +1,7 @@
+#lang racket
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+(provide accumulate)
