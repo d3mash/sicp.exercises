201ea8f6ff2aa6a5cfad4f0eb3cad90fbcde5c42
Demid Kashirin <demid.kashirin@gmail.com>
2018-06-24 17:41:15 +0300

    fix task not matching description

--- Chapter 2/2.41/test.rkt
+++ Chapter 2/2.41/test.rkt
@@ -2,5 +2,5 @@
 (require test-engine/racket-tests)
 (require (file "exercise.rkt"))
 
-(check-expect (triplets-with-sum 6 8) '((4 3 1) (5 2 1)))
+(check-expect (triplets-with-sum 6 8) '((1 1 6) (1 2 5) (1 3 4) (1 4 3) (1 5 2) (1 6 1) (2 1 5) (2 2 4) (2 3 3) (2 4 2) (2 5 1) (3 1 4) (3 2 3) (3 3 2) (3 4 1) (4 1 3) (4 2 2) (4 3 1) (5 1 2) (5 2 1) (6 1 1)))
 (test)
