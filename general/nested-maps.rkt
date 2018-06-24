c2c84717f2484ec5095b230e7a7c941bb2179b32
Demid Kashirin <demid.kashirin@gmail.com>
2018-06-24 15:48:15 +0300

    simplify sum-pairs-prime with unique-pairs

--- general/nested-maps.rkt
+++ general/nested-maps.rkt
@@ -2,7 +2,12 @@
 (require (file "accumulate.rkt"))
 (require (file "enumerate-interval.rkt"))
 (require math/number-theory)
-
+(define (unique-pairs n)
+  (define interval (enumerate-interval 1 n))
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           interval))
             
 (define (flatmap proc seq)
   (accumulate append null (map proc seq)))
@@ -13,9 +18,6 @@
 (define (prime-sum-pairs n)
   (map make-pair-sum
        (filter prime-sum?
-               (flatmap
-                (lambda (i)
-                  (map (lambda (j) (list i j))
-                       (enumerate-interval 1 (- i 1))))
-                (enumerate-interval 1 n)))))
+               (unique-pairs n))))
+(prime-sum-pairs 6)
 (provide (all-defined-out))
