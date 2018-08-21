ade161593ab33ccba921b806d5aeb6f66654f4ce
Demid Kashirin <demid.kashirin@gmail.com>
2018-08-21 16:34:07 +0300

    add a corner case to #60 test

--- Chapter 2/2.60/test.rkt
+++ Chapter 2/2.60/test.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require test-engine/racket-tests)
 (require "exercise.rkt")
+(check-expect (union '() '()) '())
 (check-expect (union '(1 2 3 4 5) '(4 5 3 6 7)) '(1 2 3 4 5 6 7))
 (check-expect (intersection '(1 3 4 5) '(3 4)) '(3 4))
 
