4d55acc76e3f709fb6d8717225cbd5b5286dbbf2
Demid Kashirin <demid.kashirin@gmail.com>
2018-09-03 00:28:21 +0300

    fix typo

--- Chapter 2/sets/treeSets.rkt
+++ Chapter 2/sets/treeSets.rkt
@@ -24,7 +24,7 @@
                     (left-branch set)
                     (adjoin-set x (right-branch set))))))
 
-(provide empty
+(provide entry
          right-branch
          left-branch
          make-tree
