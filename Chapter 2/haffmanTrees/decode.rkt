856f891447cf10ea254cce8af2e6b420fa74351d
Demid Kashirin <demid.kashirin@gmail.com>
2018-09-03 00:27:57 +0300

    add Haffman trees book examples

---
+++ Chapter 2/haffmanTrees/decode.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(require "primitives.rkt")
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit" bit))))
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? branch)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(provide decode
+         choose-branch)
